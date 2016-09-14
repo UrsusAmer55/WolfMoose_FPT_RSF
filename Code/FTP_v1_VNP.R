8d34aace4e0e473f11a53eb172570171e142bacd
UrsusAmer55 <mark.ditmer@gmail.com>
2016-09-13 19:56:24 -0500

    Raw GPS locs - all that had any activity?

--- Code/FTP_v1_VNP.R
+++ Code/FTP_v1_VNP.R
@@ -1,8 +1,151 @@
 library(adehabitatLT)
+library(bcpa)
+library(intervals)
+require(waddle)
+library(chron)
+library(spatstat)
+library(raster)
+library(ggplot2)
+library(gtools)
+library(sp)  # classes for spatial data
+library(raster)  # grids, rasters
+library(rasterVis)  # raster visualisation
+library(maptools)
+library(rgeos)
+library(geosphere)
+library(rgdal)
+library(chron)
+
+M1<-read.csv("E:/Moose/Code_Projects/VNP/Output/AllMooseLocs.csv",header=TRUE)
+
+###remove: Collar_ID: 47695, 31188, not from VNP study
+M2<-M1[c(M1$Collar_ID!=31188),]
+unique(M1$Uniq_ID)
+
+M2$Time <- chron(times=as.character(M2$Time))
+M2$Date<- as.POSIXct(M2$Date, format= "%m/%d/%Y ")
+M2$dtm <- as.factor(paste(M2$Date, M2$Time, sep = " "))
+M2$dtPm<-strptime(M2$dtm, "%Y-%m-%d %H:%M:%S")
+# ####convert to local time
+M2$dtPm <- as.POSIXct(M2$dtPm, tz="GMT")
+M2$dtPmL<-format(M2$dtPm, tz="America/Chicago",usetz=TRUE)
+M2$dtPmL<-M2$EndTimeL
+
+###remove locs with large HDOP
+M3<-M2[c(M2$HDOP<15),]
 
 
-#read in data with interp points
-M3spALLI3<-readRDS("E:/Moose/StateSpaceOutput/Combo_AllMooseCovar_072816.Rda")
+####convert to local time
+M3$dtPm <- as.POSIXct(M3$dtPm, tz="GMT")
+M3$dtPmL<-format(M3$dtPm, tz="America/Chicago",usetz=TRUE)
+M3$dtPmL<-as.POSIXct(M3$dtPmL)
+
+M3$dtPmDiff<-c(0,diff(M3$dtPm))
+
+
+M3$Uniq_IDN<-as.numeric(M3$Uniq_ID)
+M3<-M3[order(M3$Uniq_IDN,M3$dtPmL),]
+M3$obsID<-1:nrow(M3)
+
+###rename things for the waddle interpolate package
+M3$Time<-as.POSIXct(M3$dtPmL)
+M3$X<-M3$easting
+M3$Y<-M3$northing
+M3$id<-M3$IDcollar
+M3$IDcollar<-paste(M3$Uniq_ID,M3$Collar_ID,sep="_")
+
+
+M3<-M3[c(-26,-27,-28,-29,-30,-31,-32,-33)]
+
+###fix issue of 38 and 39 being mixed up
+M3$IDflip<-M3$Uniq_ID
+M3$IDflip[M3$Uniq_ID==38] <- 39
+M3$IDflip[M3$Uniq_ID==39] <- 38
+M3$Uniq_ID<-M3$IDflip
+
+M3_8<-M3[M3$Uniq_ID==8,]
+M3_8<-M3_8[M3_8$Collar_ID==99210,]
+unique(M3_8$Collar_ID)
+M3_8$dtPmLDiff<-c(0,diff(M3_8$dtPmL))
+M3_8_50<-M3_8[M3_8$dtPmLDiff>62,]
+
+
+###remove 1st fix from 45 (remove first few days) & 73 (weird first fix time)
+M373<-M3[M3$Uniq_ID==73,]
+M373<-M373[M373$dtPmL>="2012-03-15 10:30:19",]
+M3<-M3[M3$Uniq_ID!=73,]
+M3<-rbind(M3,M373)
+
+M345<-M3[M3$Uniq_ID==45,]
+head(M345,50)
+M345<-M345[M345$Uniq_ID==45&M345$dtPmL>="2011-01-28 08:36:15",]
+M3<-M3[M3$Uniq_ID!=45,]
+M3<-rbind(M3,M345)
+
+
+###what's up with the really fast move rates of 41
+M341<-M3[M3$Uniq_ID==41,]
+par(mfrow=c(1,1))
+hist(M341$steplength,freq=FALSE)
+hist(M345$steplength,freq=FALSE)
+hist(M373$steplength,freq=FALSE)
+
+M341<-M341[order(M341$dtPmL),]
+
+XY<- project(cbind( M341$Longitude,M341$Latitude), "+proj=utm +zone=15 ellps=WGS84")
+plot(XY)
+M341$Y_test<-XY[,2]
+M341$X_test<-XY[,1]
+tail(sort(M341$X_test))
+
+###look at outliers
+M341<-M341[M341$X_test<=553024,]
+plot(M341$X_test,M341$Y_test)
+
+# Distances
+nobs<-nrow(M341)  
+M341$dist_test<-c(0,sqrt((M341$X_test[-1]-M341$X_test[-nobs])^2+(M341$Y_test[-1]-M341$Y_test[-nobs])^2))
+
+par(mfrow=c(1,1))
+plot(M341$dtPmL,M341$steplength)
+hist(M341$dist_test)
+
+###looks fixed to me - replace original data with this
+M341$steplength<-c(NA,sqrt((M341$X_test[-1]-M341$X_test[-nobs])^2+(M341$Y_test[-1]-M341$Y_test[-nobs])^2))
+head(M341)
+M341$northing<-M341$Y_test
+M341$easting<-M341$X_test
+
+M341<-M341[c(1:32)]
+head(M341)
+M3<-M3[M3$Uniq_ID!=41,]
+
+names(M341)
+M341<-M341[,1:31]
+M3<-rbind(M3,M341)
+
+
+
+####2010 had 15 min intervals....
+M3$Time<-as.POSIXct(M3$dtPm)
+M3$X<-M3$easting
+M3$Y<-M3$northing
+M3$id<-M3$IDcollar
+M3$IDcollar<-paste(M3$Uniq_ID,M3$Collar_ID,sep="_")
+M3<-M3[order(M3$Uniq_IDN,M3$dtPmL),]
+
+M3_15<-M3[M3$dtPm<"2011-01-24 00:00:00",]
+M3_15<-M3_15[M3_15$dtPmDiff>=0,]
+M3_15$year<-format(M3_15$dtPmL,"%Y")
+
+
+M3_20<-M3[M3$dtPm>="2011-01-24 00:00:00",]
+M3_20<-M3_20[M3_20$dtPmDiff>=0,]
+M3_20$year<-format(M3_20$dtPmL,"%Y")
+
+
+
+
 str(M3spALLI3)
 #needed to make traj in ade
 #EndTimeL,IDcollar, X, Y
@@ -58,9 +201,6 @@ VNPmDF<-ld(VNPm)
 str(VNPmDF)
 ??gBuffer
 
-install.packages("rgeos")
-library(rgeos)
-library(sp)
 
 # points from scratch
 VNPmDF$ptid<-1:nrow(VNPmDF)
