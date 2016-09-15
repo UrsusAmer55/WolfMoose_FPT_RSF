81c5a3ea0f3e82ca506ca22515ee623bf6eb7f65
UrsusAmer55 <mark.ditmer@gmail.com>
2016-09-15 17:02:19 -0500

    updated end 5pm 09/15/16

--- Code/FTP_v1_VNP.R
+++ Code/FTP_v1_VNP.R
@@ -205,7 +205,7 @@ str(VNPmDF)
 
 # points from scratch
 VNPmDF$ptid<-1:nrow(VNPmDF)
-VNPmDF<-VNPmDF[1:300,]
+VNPmDF<-VNPmDF[1:1000,]
 coords = cbind(VNPmDF$x, VNPmDF$y)
 sp = SpatialPoints(coords)
 # make spatial data frame
@@ -216,12 +216,74 @@ str(VNPmDFsp)
 pc1100km <- gBuffer(VNPmDFsp, width=320,byid=TRUE )
 
 plot(pc1100km)
+plot(VNPm[1],add=TRUE)
 plot(VNPmDFsp,add=TRUE)
-plot(VNPm[[1]])
-
+plot(VNPm[1])
+plot(pc1100km,add=TRUE)
 # Add data, and write to shapefile
-pc100km <- SpatialPolygonsDataFrame( pc100km, data=pc100km@data )
-writeOGR( pc100km, "pc100km", "pc100km", driver="ESRI Shapefile" )
+pc100km2 <- SpatialPolygonsDataFrame( pc1100km, data=pc1100km@data )
+head(pc100km2)
+#writeOGR( pc100km, "pc100km", "pc100km", driver="ESRI Shapefile" )
+vegR<-raster("E:/Moose/Data/OutsideSpatial/NLCD11VNPclip1.tif") #raster: NLCD habitat
+
+
+###can use points and buffer with the "buffer" value in extract function
+###look into functions "zonal"
+#we will want both the distance from point to 
+#and the length inside of the following polylines:
+#1)roads and/ or snowmob trails
+#lakes
+#ponds
+#lakes and ponds
+#habitat edges
+
+#we will also want temp included if important to wolves?
+
+
+system.time(testEx2<-extract(vegR,VNPmDFsp[1:1000,],buffer=320))
+system.time(testEx<-extract(vegR,pc100km2))
+
+head(testEx2)
+data.frame(sapply(testEx2, mean))
+testout<-unlist(testEx2)
+head(testout)
+
+library(reshape2)
+
+head(melt(testEx2),10)
+testEx2df<-melt(testEx2)
+
+
+str(testEx2df)
+
+library(dplyr)
+
+sub<-testEx2df %>%
+  group_by('L1','value') %>%
+  mutate(count = n())
+
+sub<-aadt %>%
+  select(FID_b30021,FID_AADT_T,CURR_VOL) %>%
+  group_by(FID_b30021) %>%
+  mutate(Rd.count=n_distinct(FID_AADT_T)) %>%
+  mutate(maxV=max(CURR_VOL)) %>%
+  mutate(meanV=mean(CURR_VOL))
+
+L1<-testEx2df$L1
+value<-testEx2df$value
+ddply(testEx2df,L1, transform, count = length(value))
+y <- xtabs(~ L1 + value, testEx2df)
+x <- count(testEx2df, c('L1', 'value'))
+y<-as.data.frame(y)
+w <- reshape(y, 
+             timevar = "value",
+             idvar = c("L1"),
+             direction = "wide")
+head(w)
+str(w)
+w<-as.data.frame(w)
+w2 <- data.frame(matrix(unlist(w), nrow=1000, byrow=T))
+head(w2)
 
 
 
